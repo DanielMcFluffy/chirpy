7ace4daf3a802201145c6862be970ff22a791470
DanielMcFluffy <daniel001.dhf@gmail.com>
2025-05-31 23:57:16 +0800

    add basic handlers

--- main.go
+++ main.go
@@ -9,12 +9,22 @@ func main() {
   fmt.Println("HEllo world")
 
   mux := http.NewServeMux()
+
+  fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+  mux.Handle("/app/", fileServerHandler)
+
+  mux.HandleFunc("/healthz", func(res http.ResponseWriter, req *http.Request) {
+    res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    res.WriteHeader(200)
+    res.Write([]byte("OK"))
+  })
+
   server := http.Server{
     Handler: mux,
     Addr: ":8080",
   }
   if err := server.ListenAndServe(); err != nil {
-    fmt.Errorf("Unable start server: %w", err)
+    fmt.Println("Unable start server: %w", err)
   }
 
 }
